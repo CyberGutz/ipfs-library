956e3ac5384afd33e74cf34293a3ee47ac1a3335
Gustavo Magalhães <gustavotmlhaes@gmail.com>
2026-05-13 23:32:03 -0300

    added search, home and pages

---
+++ ipfs-library/src/lib/backendSelector.js
@@ -0,0 +1,2 @@
+const { getIpfs, providers } = require('ipfs-provider');
+	
--- ipfs-library/src/routes/+page.svelte
+++ ipfs-library/src/routes/+page.svelte
@@ -3,25 +3,92 @@ import { parseCSV } from '$lib/csvParse';
 
 const url = "http://localhost:8080/ipfs/QmNcou4jH6m3Jd6H5orQDWzoUVXsX8e6YmLsYcoCUhQUAB";
 
+const mod = 500;
+let pesquisa = $state('');
+let start = $state(0);
+let end = $state(mod);
+let acervo = $state([]);
+let exibido = $state([]);
+
 let fetchPromise = fetch(url)
 	.then(response => response.text())
-	.then(text =>{
-		return parseCSV(text)
+	.then(async text =>{
+		const parsed = await parseCSV(text);
+		console.log("o que o parser retornou: ", typeof(parsed));
+		acervo = parsed?.value || parsed;
+		console.log("tipo do acervo",typeof(acervo));
+		console.log("Acervo: ",acervo);
+		exibido = acervo.slice(0,mod);
+		return acervo;
 	});
+
+function buscar(){
+	if (!pesquisa.trim()){
+		returnHome();
+		return;
+	}
+	
+	start = 0;
+	end = mod;
+	exibido = acervo.filter(livro => livro.title.toLowerCase().includes(pesquisa.toLowerCase()));
+}
+
+function returnHome(){
+	start = 0;
+	end = mod;
+	pesquisa = '';
+	exibido = acervo.slice(0,mod);
+	return;
+}
+
+function nextPage(){
+	if (end < acervo.length){
+		start += mod;
+		end += mod;
+		exibido = acervo.slice(start, end)
+	}
+	return;
+}
+
+function prevPage(){
+	if (start > 0){
+		start -= mod;
+		end -= mod;;
+		exibido = acervo.slice(start, end)
+	}
+	return;
+}
+
+
+// let acervoFiltrado = $derived(
+// 	pesquisa ? acervo.filter(d => d.title?.toLowerCase().includes(pesquisa.toLowerCase())) : acervo
+// )
 
 </script>
 
 {#await fetchPromise}
-...
-{:then dados}
+Carregando acervo...
+{:then _}
+	<button onclick={returnHome}>home</button>
+	<input type="search" id="search" bind:value={pesquisa} placeholder="Bram Stoker's Dracula" onkeydown={(e) => e.key === 'Enter' && buscar()}/>
+	<button onclick={buscar}>search</button>
+	<button onclick={prevPage}>Página anterior</button>
+	<button onclick={nextPage}>Proxima página</button>
+
+	<hr/>
 
 	<ul>
-		{#each dados as linha}
-			<li><a href="http://localhost:8080/ipfs/{linha.cid}">{linha.title}</a></li>
-		{/each}
+		{#if exibido.length <= 0}
+			Livro não encontrado!
+			<button onclick={returnHome}>retorne ao acervo</button>
+		{:else}
+			{#each exibido as linha (linha.id)}
+				<li><a href="http://localhost:8080/ipfs/{linha.cid}" download="{linha.title}.txt">{linha.title}</a></li>
+			{/each}
+		{/if}
 	</ul>
 
-	{:catch erro}
-		<p> deu ruim </p>
+{:catch erro}
+	<p> deu ruim </p>
 {/await}
 
