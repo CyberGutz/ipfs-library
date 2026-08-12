efc5862bff7935fe618c653ed925e0d9bd9941c8
Gustavo Magalhães <gustavotmlhaes@gmail.com>
2026-08-12 20:17:29 -0300

    frontend iniciado

--- ipfsbooks/src/routes/+page.svelte
+++ ipfsbooks/src/routes/+page.svelte
@@ -1,54 +1,55 @@
 <script>
-    import { catalogSync, bookList, searchBooks, db } from "$lib/dbInstance";
-    import { readCID, downloadCID, pinCID } from "$lib/ipfsOperations";
-    import { onMount } from "svelte";
+	import { catalogSync, bookList, searchBooks, db } from "$lib/dbInstance";
+	import { onMount } from "svelte";
+	import BookPage from "./BookPage.svelte";
 
-    let books= $state([]);
-    let page = $state(0);
-    let carregando = $state(true);
+	let books = $state([]);
+	let selecionado = $state();
+	let page = $state(0);
+	let carregando = $state(true);
 
-    
-    onMount(async () =>{
-        await catalogSync();
-	books = await bookList(page,50);
-	carregando = false;
-    })
+	onMount(async () => {
+		await catalogSync();
+		books = await bookList(page, 50);
+		carregando = false;
+	});
 
-    async function nextPage(){
-	page++;
-	const newBooks = await bookList(page,50);
-	books = [...books, ...newBooks];
+	async function nextPage() {
+		page++;
+		const newBooks = await bookList(page, 50);
+		books = [...books, ...newBooks];
 	}
 
-    async function prevPage(){
-    	if(page + 50 < 0)
-		return;
-	page--;
-	const newBooks = await bookList(page,50);
-	books = [...books, ...newBooks];
+	async function prevPage() {
+		if (page + 50 < 0) return;
+		page--;
+		const newBooks = await bookList(page, 50);
+		books = [...books, ...newBooks];
 	}
-
 </script>
 
 <h2>IPFSBooks!</h2>
 
 {#if carregando}
-<p>Sincronizando o catálogo, aguarde um momento...</p>
+	<p>Sincronizando o catálogo, aguarde um momento...</p>
 {:else}
-<header>
-	<p>Books to anyone, free of corporate bullshit!</p>
-	<button onclick={prevPage}>Previous</button>
-	<button onclick={nextPage}>Next</button>
-</header>
-{#each books as book}
-<li>
-	{book.titulo} 
-	<button onclick={()=> downloadCID(book.cid, book.titulo, "text/plain")}>
-		download
-	</button>
-</li>
-{/each}
+	<header>
+		<p>Books to anyone, free of corporate bullshit!</p>
+		<button onclick={()=> selecionado = null}>Home</button>
+	</header>
+	{#if selecionado != null}
+		<BookPage {selecionado}/>
+	{:else}
+		{#each books as book}
+			<div>
+				<a href="localhost:8080" onclick={()=>{
+					selecionado = book;
+					return false;
+				}}>
+					{book.titulo}
+				</a>
+				<!-- <span> <button onclick={()=> downloadCID(book.cid, book.titulo, "text/plain")}>download</button></span> -->
+			</div>
+		{/each}
+	{/if}
 {/if}
-
-
-
---
+++ ipfsbooks/src/routes/BookPage.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+	let {selecionado} = $props();
+	import { downloadCID } from "../lib/ipfsOperations";
+</script>
+
+<div>
+{selecionado.titulo}
+</div>
+<div>
+	<div>
+		Capa			
+	</div>
+	<div>
+		<div>Autor: {selecionado.autor}</div>
+		<div>Data de Lançamento: {selecionado.ano}</div>
+		<div>Língua: {selecionado.lingua}</div>
+		<div>ISBN: {selecionado.isbn}</div>	
+		<div>Categorias: {selecionado.categorias}</div>
+	</div>
+</div>
+<p>Links de Download</p>
+<div>Baixar livro como um arquivo de texto: 
+	<button onclick={()=> downloadCID(selecionado.cid, selecionado.titulo, "text/plain")}>Download!</button>
+</div>
+<div>
+
+</div>
+<div>
+
+</div>
